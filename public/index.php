b2154750e5b11adad41e6f86af6c1fcbe9188e3b
jyotirmoy1997 <subhodas45036@gmail.com>
2024-02-12 19:41:00 +0530

    Refactored routing code : relocated routing code to routes.php, relocated router loading code to router.php

--- public/index.php
+++ public/index.php
@@ -1,23 +1,9 @@
 <?php
     require '../helpers.php';
-    // loadView('home');
 
-    $routes = [
-        '/workspace/public/' => 'controllers/home.php',
-        '/workspace/public/listings' => 'controllers/listings/index.php',
-        '/workspace/public/listings/create' => 'controllers/listings/create.php',
-        '404' => 'controllers/errors/404.php'
-    ];
-
+    $routes = require basePath('routes.php');
     $uri = $_SERVER["REQUEST_URI"];
 
-    // inspectAndDie($uri);
-
-    if(array_key_exists($uri, $routes)){
-        require(basePath($routes[$uri]));
-    }
-    else{
-        require(basePath($routes['404']));
-    }
+    require basePath('router.php')
 
 ?>
---
+++ routes.php
@@ -0,0 +1,10 @@
+<?php
+
+    return [
+        '/workspace/public/' => 'controllers/home.php',
+        '/workspace/public/listings' => 'controllers/listings/index.php',
+        '/workspace/public/listings/create' => 'controllers/listings/create.php',
+        '404' => 'controllers/errors/404.php'
+    ];
+
+?>
